d49a9833c71f5f7c396feae9484690173540a187
JoonHyeongLEE <ryzenk1207@gmail.com>
2021-08-07 16:35:32 +0900

    Arduino timer isr led toggle

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -3,19 +3,18 @@
 int CNT = 0;
 int CNT2 = 0;
 
+#define LED 13
+
 void setup()
 {
   TimerOne.attachInterrupt( Timer_ISR );
-  TimerOne.Initalize
+  TimerOne.Initalize(100000);
   
+  pinMode(LED,OUTPUT);
 
 }
 
 void Timer_ISR ()
 {
-  
-}
-void loop()
-{
- 
+  digitalWrite(LED, digitalRead(LED) ^ 1);
 }
